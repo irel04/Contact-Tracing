be54c3d326ea346004b737f83d300ca22142806f
Mark Lhester Agellon <kianirel56@gmail.com>
2022-11-13 00:07:37 +0800

    Finish Lab Exercise

--- Contact Tracing.py
+++ Contact Tracing.py
@@ -4,31 +4,13 @@ print(f"{'=' * 75}")
 
 # Create an empty dictionary
 contacts ={
-    "lite":{
-        "number": "luh",
-        "address": "luh",
-        "cough/cold/fever?": "luh",
-        "contact-with-covid-19?": "yes"},
-    "Mary":{
-            "number": "09327893355",
-            "address": "luh",
-            "cough/cold/fever?": "luh",
-            "contact-with-covid-19?": "no"},
-  "FB":{
-        "number": "luh",
-        "address": "luh",
-        "cough/cold/fever?": "luh",
-        "contact-with-covid-19?": "yes"},
-   "good":{
-        "number": "luh",
-        "address": "luh",
-        "cough/cold/fever?": "luh",
-        "contact-with-covid-19?": "no"}      
+     
 
 }
 
-def creating_database(x, y, z, a, b):
-    contacts[x] = {
+def creating_database(w,x, y, z, a, b):
+    contacts[w] = {
+        "age": x,
         "number": y,
         "address": z,
         "cough/cold/fever?": a,
@@ -45,33 +27,36 @@ print("[d] Exit")
 while True:
     # Prompt the user about the options
     option = input("\nChoose an option from (a-c): ")
-
+    
     # Option 1 (add user information such as full name, contacts, address, etc.)
     if option.lower() == "a":
         print("\nPlease fill up the necessary information for contact tracing.")
         name = input("Enter your full name: ")
+        age = input("Enter your age: ")
         number = input("Enter your number: ")
         address = input("Enter your address: ")
         condition =input("Are you experiencing cough, cold, or fever? (yes/no): ")
         in_contact = input("Do you had contact with a person with Covid? (yes/no): ")
-        creating_database(name, number, address, condition, in_contact)
+        creating_database(name, age, number, address, condition, in_contact)
+        print("\nDATA SAVED SUCCESSFULLY!!!")
 
     # Option 2 (search user record)
     elif option.lower() == "b":
         search = input("\nEnter name you wanted to search: ")
         for list in contacts:
             if list == search:
+                print("age:", contacts[search]["age"])
                 print("number:", contacts[search]["number"])
                 print("address:", contacts[search]["address"])
                 print("cough/cold/fever:", contacts[search]["cough/cold/fever?"])
                 print("contact-with-covid-19:", contacts[search]["contact-with-covid-19?"])
+            
 
 
     # Option 3 (View all contacts)
     elif option.lower() == "c":
         print()
-        print("List of names in the contacts")
-        print("\nBased on the database, {} persons are listed below:".format(str(len(contacts))))
+        print("Based on the database, {} persons are listed below:\n".format(str(len(contacts))))
         for i in contacts:
             print(i)
 
@@ -85,3 +70,7 @@ while True:
         elif retry.lower() == "n":
             break
     # Create a Loop 
+    else:
+        print("invalid input!!!")
+        continue
+    print("\n----------------------Line Break----------------------------")
